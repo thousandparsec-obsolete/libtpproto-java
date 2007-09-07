64b6f0b4d3b78c8937c01492532c527ed68c6ccb
Krzysztof Sobolewski <jezuch@interia.pl>
2007-09-07 21:37:17 +0200

    Fetch objects in ObjectIterator in chunks.

--- src-protohelpers/net/thousandparsec/netlib/tp03/ObjectIterator.java
+++ src-protohelpers/net/thousandparsec/netlib/tp03/ObjectIterator.java
@@ -1,6 +1,10 @@
 package net.thousandparsec.netlib.tp03;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import net.thousandparsec.netlib.Connection;
 import net.thousandparsec.netlib.TPException;
@@ -8,26 +12,63 @@ import net.thousandparsec.netlib.tp03.GetWithID.IdsType;
 import net.thousandparsec.util.AbstractIterator;
 
 /**
- * This class is an {@link java.util.Iterator} that produces {@link Object}
- * frames for each object (in order as returned by the server).
+ * This class is an {@link Iterator} that produces {@link Object} frames for
+ * each object (in order as returned by the server).
  * 
  * @author ksobolewski
  */
 public class ObjectIterator extends AbstractIterator<Object>
 {
 	private final Connection<TP03Visitor> conn;
-	private final SimpleObjectIterator oit;
-	private Object next;
+	private final int chunkSize;
+	private final SimpleObjectIterator objIds;
+	private List<Object> next=new LinkedList<Object>();
+	private Iterator<Object> nextIter=Collections.<Object>emptyList().iterator();
 
 	public ObjectIterator(Connection<TP03Visitor> conn, int chunkSize)
 	{
 		this.conn=conn;
-		this.oit=new SimpleObjectIterator(conn, chunkSize);
+		this.chunkSize=chunkSize;
+		this.objIds=new SimpleObjectIterator(conn, chunkSize);
 	}
 
-	void setNext(Object next)
+	void addNext(Object next)
 	{
-		this.next=next;
+		this.next.add(next);
+	}
+
+	private boolean fetchObjects() throws IOException, TPException
+	{
+		GetObjectsByID getObj=new GetObjectsByID();
+		int count;
+		for (count=0; count < chunkSize && objIds.hasNext(); count++)
+			getObj.getIds().add(new IdsType(objIds.next().getID()));
+		if (count == 0)
+			return false;
+
+		final int toGet=count;
+		conn.sendFrame(getObj, new TP03Visitor(true)
+			{
+				@Override
+				public void frame(Sequence frame)
+				{
+					assert frame.getNumber() == toGet;
+				}
+			});
+		assert next.isEmpty();
+		for (int i=0; i < toGet; i++)
+			conn.receiveFrame(new TP03Visitor(true)
+				{
+					@Override
+					public void frame(Object frame)
+					{
+						addNext(frame);
+					}
+				});
+		assert next.size() == toGet;
+
+		nextIter=next.iterator();
+		return true;
 	}
 
 	@Override
@@ -35,32 +76,11 @@ public class ObjectIterator extends AbstractIterator<Object>
 	{
 		try
 		{
-			if (oit.hasNext())
+			if (nextIter.hasNext() || fetchObjects())
 			{
-				//FIXME: fetch objects in larger chunks
-				int id=oit.next().getID();
-				GetObjectsByID getObj=new GetObjectsByID();
-				getObj.getIds().add(new IdsType(id));
-
-				conn.sendFrame(getObj, new TP03Visitor(true)
-					{
-						@Override
-						public void frame(Sequence frame)
-						{
-							assert frame.getNumber() == 1;
-						}
-					});
-				next=null;
-				conn.receiveFrame(new TP03Visitor(true)
-					{
-						@Override
-						public void frame(Object frame)
-						{
-							setNext(frame);
-						}
-					});
-				assert next != null;
-				return next;
+				Object ret=nextIter.next();
+				nextIter.remove();
+				return ret;
 			}
 
 			finished();
@@ -82,7 +102,7 @@ public class ObjectIterator extends AbstractIterator<Object>
 		Connection<TP03Visitor> conn=new TP03Decoder().makeConnection(new URI("tp://guest:guest@demo1.thousandparsec.net/tp"), true);
 		try
 		{
-			for (Iterator<Object> oit=new ObjectIterator(conn, 100); oit.hasNext(); )
+			for (Iterator<Object> oit=new ObjectIterator(conn, 10); oit.hasNext(); )
 			{
 				Object object=oit.next();
 				System.out.printf("id=%d, type=%d, name=%s%n", object.getId(), object.getOtype(), object.getName());
