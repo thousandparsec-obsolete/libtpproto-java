23a61bcec4c5743f2d3185065ca0213ec13a0748
Krzysztof Sobolewski <jezuch@interia.pl>
2007-09-07 20:02:20 +0200

    Modify TestLoopback to create, send and receive all possible frames, getting the list by reflection from TP03Visitor class (all methods named "frame").

--- src-test/net/thousandparsec/netlib/test/TestLoopback.java
+++ src-test/net/thousandparsec/netlib/test/TestLoopback.java
@@ -1,75 +1,74 @@
 package net.thousandparsec.netlib.test;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.Socket;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import net.thousandparsec.netlib.Connection;
 import net.thousandparsec.netlib.Frame;
 import net.thousandparsec.netlib.FrameDecoder;
-import net.thousandparsec.netlib.tp03.GetMessage;
-import net.thousandparsec.netlib.tp03.Login;
 import net.thousandparsec.netlib.tp03.TP03Decoder;
 import net.thousandparsec.netlib.tp03.TP03Visitor;
-import net.thousandparsec.netlib.tp03.GetWithIDSlot.SlotsType;
 
 public class TestLoopback extends TP03Visitor
 {
-	public static void main(String[] args) throws IOException, InterruptedException
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws IOException, InterruptedException, InstantiationException, IllegalAccessException, ExecutionException
 	{
+		List<Frame<TP03Visitor>> frames=new ArrayList<Frame<TP03Visitor>>();
+		for (Method m : TP03Visitor.class.getMethods())
+		{
+			if (m.getName().equals("frame"))
+			{
+				Class<?> fc=m.getParameterTypes()[0];
+				//don't do this at home
+				frames.add((Frame<TP03Visitor>)fc.newInstance());
+			}
+		}
+
 		FrameDecoder<TP03Visitor> ff=new TP03Decoder();
 		Connection<TP03Visitor> conn=new Connection<TP03Visitor>(ff, new DebugSocket());
 
-		Future<Void> asyncTask=conn.receiveAllFramesAsync(new TestLoopback());
+		Future<Void> asyncTask=conn.receiveAllFramesAsync(new TestLoopback(frames));
 
 		try
 		{
-			conn.sendFrame(new Login());
-			System.out.println("Frame sent");
-
-			GetMessage object=new GetMessage();
-			object.setId(5);
-			object.getSlots().addAll(Arrays.asList(
-				new SlotsType(13),
-				new SlotsType(14),
-				new SlotsType(15),
-				new SlotsType(16),
-				new SlotsType(17),
-				new SlotsType(18)));
-			for (int i=0; i < 100; i++)
+			for (Frame<TP03Visitor> frame : frames)
 			{
-				conn.sendFrame(object);
-				System.out.println("Frame sent");
+				conn.sendFrame(frame);
+				assert !asyncTask.isDone();
+				System.out.println("Sent frame: "+frame.getFrameType()+" ("+frame+")");
 			}
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace(System.err);
 		}
 		finally
 		{
 			try {conn.close();} catch (IOException ignore) {}
 			//get the null from task just to see if there was an exception
-			try
-			{
-				//but don't swallow the exception that could bring us to this finally block
-				asyncTask.get();
-			}
-			catch (ExecutionException ex)
-			{
-				ex.getCause().printStackTrace(System.err);
-			}
+			//exception thrown from here will mask any exceptions that could bring us here
+			//but we don't mind, because exception means the test failed anyway
+			asyncTask.get();
 		}
+	}
+
+	private final List<Frame<TP03Visitor>> frames;
+
+	public TestLoopback(List<Frame<TP03Visitor>> frames)
+	{
+		this.frames=frames;
 	}
 
 	@Override
 	public void unhandledFrame(Frame<?> frame)
 	{
-		System.out.println("Unhandled frame: "+frame.getFrameType()+" ("+frame+")");
-	}
-
-	@Override
-	public void frame(GetMessage o)
-	{
-		System.out.println("VISIT GetMessage!");
+		System.out.println("Received frame: "+frame.getFrameType()+" ("+frame+")");
 	}
 
 	private static class DebugSocket extends Socket
