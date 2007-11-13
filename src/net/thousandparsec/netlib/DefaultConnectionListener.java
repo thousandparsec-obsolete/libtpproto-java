355a044f706c809a9719be4119fc401d02984944
Krzysztof Sobolewski <jezuch@interia.pl>
2007-11-13 21:56:17 +0100

    Make DefaultConnectionListener more flexible - add three flags to print sent frames and/or received frames and/or errors. By default all three are set.

--- src/net/thousandparsec/netlib/DefaultConnectionListener.java
+++ src/net/thousandparsec/netlib/DefaultConnectionListener.java
@@ -7,24 +7,49 @@ package net.thousandparsec.netlib;
  */
 public class DefaultConnectionListener<V extends Visitor> implements ConnectionListener<V>
 {
+	private final boolean printSent;
+	private final boolean printReceived;
+	private final boolean printError;
+
+	public DefaultConnectionListener(boolean printSent, boolean printReceived, boolean printError)
+	{
+		this.printSent=printSent;
+		this.printReceived=printReceived;
+		this.printError=printError;
+	}
+
+	public DefaultConnectionListener()
+	{
+		this(true, true, true);
+	}
+
 	public void frameSent(ConnectionEvent<V> ev)
 	{
-		Frame<V> frame=ev.getFrame();
-		System.err.printf("%s: Sent frame seq %d, type %d (%s)%n", getClass().getName(), frame.getSequenceNumber(), frame.getFrameType(), frame.toString());
+		if (printSent)
+		{
+			Frame<V> frame=ev.getFrame();
+			System.err.printf("%s: Sent frame seq %d, type %d (%s)%n", getClass().getName(), frame.getSequenceNumber(), frame.getFrameType(), frame.toString());
+		}
 	}
 
 	public void frameReceived(ConnectionEvent<V> ev)
 	{
-		Frame<V> frame=ev.getFrame();
-		if (ev.isAsync())
-			System.err.printf("%s: Received asynchronous frame seq %d, type %d (%s)%n", getClass().getName(), frame.getSequenceNumber(), frame.getFrameType(), frame.toString());
-		else
-			System.err.printf("%s: Received frame seq %d, type %d (%s)%n", getClass().getName(), frame.getSequenceNumber(), frame.getFrameType(), frame.toString());
+		if (printReceived)
+		{
+			Frame<V> frame=ev.getFrame();
+			if (ev.isAsync())
+				System.err.printf("%s: Received asynchronous frame seq %d, type %d (%s)%n", getClass().getName(), frame.getSequenceNumber(), frame.getFrameType(), frame.toString());
+			else
+				System.err.printf("%s: Received frame seq %d, type %d (%s)%n", getClass().getName(), frame.getSequenceNumber(), frame.getFrameType(), frame.toString());
+		}
 	}
 
 	public void connectionError(ConnectionEvent<V> ev)
 	{
-		System.err.println("Connection error:");
-		ev.getException().printStackTrace(System.err);
+		if (printError)
+		{
+			System.err.println("Connection error:");
+			ev.getException().printStackTrace(System.err);
+		}
 	}
 }
