517bbf167abdacf16839cf94730f0377b862737c
Brendan Matthew Roberts <alklomion@verbal.mithis.com>
2008-08-19 03:20:17 -0400

    Added some functionality to the messages interface: messages will be viewabe and the user will be able to navigate through them with the left and right keypad buttons. It may be the case that at a later date, support for extra (num pad) keys to be used for navigation, this will be easy to change. Also added support to the messages interface to delete messages, as well as an easy way to add messages to a list to be navigated through.
    via git-CVS emulator

--- src/net/thousandparsec/netlib/Client/Messages.java
+++ src/net/thousandparsec/netlib/Client/Messages.java
@@ -8,8 +8,7 @@ package net.thousandparsec.netlib.Client;
 import org.j4me.ui.*;
 
 import org.j4me.ui.components.*;
-import javax.microedition.lcdui.Graphics;
-import java.io.IOException;
+import java.util.Vector;
 /**
  *
  * @author Brendan
@@ -20,45 +19,107 @@ public class Messages extends Dialog {
      */
     private final DeviceScreen previous;
     
-    private static final String IMAGE_LOCATION = "/net/thousandparsec/netlib/Client/placeholder.png";
-    private Picture picture = new Picture();
+    
+    /*
+     * A list of strings of messages
+     */
+    Vector messageList; 
+    /*
+     * Current message
+     */
+    String currentMessage;
+    String firstMessage="This is a Test Message\n You can use the left and right buttons on your keypad to navigate through the messages!";
+    String testStringOne = "This is the First Test String";
+    String testStringTwo = "This is the Second Message";
+    String testStringThree = "This is the Third Message";
+    String defaultMessage = "No messages to display.";
+    Label message;
     
     
     public Messages(DeviceScreen previous){
         this.previous = previous;
         setTitle("Messages");
-        setMenuText("Back", "Delete");
-        
-        /*
-         * PlaceHolder Image
-         */
-          try
-	{
-            // Center the picture.
-            picture.setHorizontalAlignment( Graphics.HCENTER );
-			
-            // The image location within the Jar.
-            picture.setImage( IMAGE_LOCATION );
-	
-            // Add the picture to this screen.
-            append( picture );
-	}
-	catch (IOException e)
-	{
-            // Show an error message instead of the picture.
-            Label error = new Label( "Error:  Could not find image " + IMAGE_LOCATION );
-            append( error );
-            //e.printStackTrace();
-	}
+        setMenuText("Back", "Delete Message");
+        messageList = new Vector();
+        messageList.addElement(firstMessage);
+        messageList.addElement(testStringOne);
+        messageList.addElement(testStringTwo);
+        messageList.addElement(testStringThree);
+        currentMessage=(String) messageList.elementAt(0);
+        message = new Label(currentMessage);
+        append(message);
+
     }
-    
-    /*public void acceptNotify(){
-        //delete or menu that contains delete?
-    }*/
     
     public void declineNotify(){
         //back button
         previous.show();
+    }
+    public void acceptNotify(){
+        //delete the currentMessage
+        removeCurrentMessage();
+    }
+    protected void keyPressed (int key)
+    {
+        //find out which key was pressed
+        updateMessage(key);
+        //continue processing the event
+        super.keyPressed(key);
+    }
+    protected void keyRepeated(int key){
+        updateMessage(key);
+        // Continue processing the key event.
+        super.keyRepeated( key );
+    }
+    /*
+     * Allows the Messages to be Navigatable
+     * 
+     */
+    protected void updateMessage(int key){
+        if ( key == LEFT){
+            if(messageList.indexOf(currentMessage)>0){
+                currentMessage = (String) messageList.elementAt(messageList.indexOf(currentMessage)-1);
+                
+                message.setLabel(currentMessage);
+                message.repaint();
+                
+            }
+        }
+        if (key == RIGHT){
+            if(messageList.indexOf(currentMessage)< messageList.size()-1){
+                currentMessage = (String) messageList.elementAt(messageList.indexOf(currentMessage)+1);
+                
+                message.setLabel(currentMessage);
+                message.repaint();
+            }
+            
+        }
+        
+	
+    }
+    public void AddMessage(String message){
+        messageList.addElement(message);
+    }
+    public void removeCurrentMessage(){
+        
+        int index = messageList.indexOf(currentMessage);
+        if(index ==-1){
+         return;
+        }
+        messageList.removeElementAt(index);
+        if(messageList.size()==0){
+            message.setLabel(defaultMessage);
+            message.repaint();
+        }
+        else{
+            //if(index == messageList.size()-1){
+                currentMessage = (String) messageList.elementAt(index);
+                message.setLabel(currentMessage);
+                message.repaint();
+            //}
+            //currentMessage = (String) messageList.elementAt(message)
+        }
+        
     }
 
 }
